47979656dcccb219ed9bec7a98877c1b03f0587e
Anshuman Chhabra <anshuman.lalakers@gmail.com>
2017-12-22 12:45:18 +0530

    Starting off

---
+++ detectors/canny_segmenter.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import os
+
+def update(x):
+    pass
+
+cv2.namedWindow('FIELD', cv2.WINDOW_NORMAL)
+
+cv2.createTrackbar('t1','FIELD',0,255, update)
+cv2.createTrackbar('t2','FIELD',0,255, update) 
+
+for f in os.listdir('samples/'):
+
+    while True:
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
+
+        image = cv2.imread('samples/'+f, cv2.IMREAD_GRAYSCALE)
+        image = cv2.bilateralFilter(image, 5, 175, 175)               
+        t1 = cv2.getTrackbarPos('t1', 'FIELD')
+        t2 = cv2.getTrackbarPos('t2', 'FIELD')
+    
+        canny = cv2.Canny(image,t1,t2)
+        
+        h,w = image.shape
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (max(10, int(0.01*h + 1)), max(10, int(0.01*h +1))))
+        canny = cv2.morphologyEx(canny, cv2.MORPH_DILATE, kernel)
+        im2,contours, heirarchy = cv2.findContours(canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        
+        cv2.drawContours(image, contours, -1, (0,255,0),3)
+        
+        image = cv2.resize(image, (1920,1080))
+        cv2.imshow('FIELD', image)
+
