7508b01b5598dd89b14010b3fd54de3dd88fc699
Anshuman Chhabra <anshuman.lalakers@gmail.com>
2017-12-22 12:48:54 +0530

    Completed automated/trackbar based canny edge detection

--- detectors/automated_canny_detector.py
+++ detectors/automated_canny_detector.py
@@ -10,14 +10,16 @@ cv2.namedWindow('FIELD', cv2.WINDOW_NORMAL)
 cv2.createTrackbar('t1','FIELD',0,255, update)
 cv2.createTrackbar('t2','FIELD',0,255, update) 
 
-for f in os.listdir('samples/'):
+direc = raw_input("Enter directory of files (Include '/' at the end): ")
+
+for f in os.listdir(direc):
 
     while True:
         k = cv2.waitKey(1) & 0xFF
         if k == 27:
             break
 
-        image = cv2.imread('samples/'+f, cv2.IMREAD_GRAYSCALE)
+        image = cv2.imread(direc+f, cv2.IMREAD_GRAYSCALE)
         image = cv2.bilateralFilter(image, 5, 175, 175)               
         t1 = cv2.getTrackbarPos('t1', 'FIELD')
         t2 = cv2.getTrackbarPos('t2', 'FIELD')
